dfa6624c86755a07d602d0b3bff82b200277c852
roman <roman.schaller@gmail.com>
2015-07-23 17:24:46 +0200

    Create constants for local db keys. see #1

--- src/main/java/to/rtc/rtc2jira/Main.java
+++ src/main/java/to/rtc/rtc2jira/Main.java
@@ -6,8 +6,6 @@ import java.util.List;
 import to.rtc.rtc2jira.exporter.Exporter;
 import to.rtc.rtc2jira.exporter.GitHubExporter;
 import to.rtc.rtc2jira.extract.RTCExtractor;
-import to.rtc.rtc2jira.mapping.DefaultMappingRegistry;
-import to.rtc.rtc2jira.mapping.DirectMapping;
 import to.rtc.rtc2jira.storage.StorageEngine;
 
 /**
@@ -17,7 +15,6 @@ import to.rtc.rtc2jira.storage.StorageEngine;
 public class Main {
 
   public static void main(String[] args) throws Exception {
-    registerDefaultMappings();
     try (StorageEngine storageEngine = new StorageEngine()) {
       RTCExtractor extractor = new RTCExtractor(Settings.getInstance(), storageEngine);
       extractor.extract();
@@ -27,13 +24,6 @@ public class Main {
         }
       }
     }
-  }
-
-  private static void registerDefaultMappings() {
-    DefaultMappingRegistry registry = DefaultMappingRegistry.getInstance();
-    registry.register("summary", new DirectMapping("summary"));
-    registry.register("description", new DirectMapping("description"));
-    registry.register("workItemType", new DirectMapping("workItemType"));
   }
 
   private static List<Exporter> getExporters(StorageEngine storageEngine) {
---
+++ src/main/java/to/rtc/rtc2jira/storage/WorkItemConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2015 BISON Schweiz AG, All Rights Reserved.
+ */
+package to.rtc.rtc2jira.storage;
+
+
+/**
+ * Data from default mappings are stored in the db with this keys.
+ * 
+ * @author roman.schaller
+ *
+ */
+public interface WorkItemConstants {
+  public static final String SUMMARY = "summary";
+  public static final String DESCRIPTION = "description";
+  public static final String WORK_ITEM_TYPE = "workItemType";
+}

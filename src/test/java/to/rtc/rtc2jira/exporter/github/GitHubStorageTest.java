5366dab6440db1656de7a6f2c2a1afabd497c66e
Manuel <wtfjoke@livenet.ch>
2015-07-31 21:25:54 +0200

    create a separate test-helper class for creating workItems

--- src/test/java/to/rtc/rtc2jira/exporter/github/GitHubStorageTest.java
+++ src/test/java/to/rtc/rtc2jira/exporter/github/GitHubStorageTest.java
@@ -3,19 +3,19 @@ package to.rtc.rtc2jira.exporter.github;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static to.rtc.rtc2jira.exporter.github.GitHubStorage.GITHUB_WORKITEM_LINK;
+import static to.rtc.rtc2jira.storage.TestWorkItemCreator.createWorkItem;
 
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicReference;
 
 import org.eclipse.egit.github.core.Issue;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import com.orientechnologies.orient.core.record.impl.ODocument;
-
 import to.rtc.rtc2jira.TestDatabaseRule;
 import to.rtc.rtc2jira.storage.StorageEngine;
+
+import com.orientechnologies.orient.core.record.impl.ODocument;
 
 /**
  * Test of {@link GitHubStorage}
@@ -38,7 +38,7 @@ public class GitHubStorageTest {
   @Test
   public void testStoreIssue_IssueIsNull_ExpectNoExceptionAndNoStorage() {
     Issue issue = null;
-    ODocument workItem = createWorkItem(1);
+    ODocument workItem = createWorkItem(1, storageEngine);
 
     storage.storeLinkToIssueInWorkItem(Optional.ofNullable(issue), workItem);
 
@@ -50,23 +50,11 @@ public class GitHubStorageTest {
   public void testStoreIssue_IssueIsGiven_ExpectLinkToIssueIsStored() {
     int expectedIssueNumber = 1337;
     Issue issue = new Issue().setNumber(expectedIssueNumber);
-    ODocument workitem = createWorkItem(2);
+    ODocument workitem = createWorkItem(2, storageEngine);
 
     storage.storeLinkToIssueInWorkItem(Optional.ofNullable(issue), workitem);
 
     Object link = workitem.field(GITHUB_WORKITEM_LINK);
     assertEquals(1337, link);
   }
-
-  private ODocument createWorkItem(int id) {
-    final AtomicReference<ODocument> reference = new AtomicReference<>();
-    storageEngine.withDB(db -> {
-      ODocument doc = new ODocument("WorkItem");
-      doc.field("ID", id);
-      doc.save();
-      reference.set(doc);
-    });
-    return reference.get();
-  }
-
 }

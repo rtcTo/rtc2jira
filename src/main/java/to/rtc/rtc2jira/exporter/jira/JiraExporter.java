b2fe717855b5796e99706cd61515dd9c96f40179
Manuel Wessner <manuel.wessner@bison-group.com>
2015-07-31 16:12:05 +0200

    implemented first version of export

--- src/main/java/to/rtc/rtc2jira/exporter/jira/JiraExporter.java
+++ src/main/java/to/rtc/rtc2jira/exporter/jira/JiraExporter.java
@@ -1,10 +1,19 @@
 package to.rtc.rtc2jira.exporter.jira;
 
+import static to.rtc.rtc2jira.storage.WorkItemConstants.DESCRIPTION;
+import static to.rtc.rtc2jira.storage.WorkItemConstants.ID;
+import static to.rtc.rtc2jira.storage.WorkItemConstants.SUMMARY;
+import static to.rtc.rtc2jira.storage.WorkItemConstants.WORK_ITEM_TYPE;
+import static to.rtc.rtc2jira.storage.WorkItemTypes.BUSINESSNEED;
+import static to.rtc.rtc2jira.storage.WorkItemTypes.EPIC;
+import static to.rtc.rtc2jira.storage.WorkItemTypes.STORY;
+import static to.rtc.rtc2jira.storage.WorkItemTypes.TASK;
+
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.Optional;
 
-import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
-
+import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.ClientResponse.Status;
 import com.sun.jersey.api.client.GenericType;
@@ -12,20 +21,22 @@ import com.sun.jersey.api.client.GenericType;
 import to.rtc.rtc2jira.Settings;
 import to.rtc.rtc2jira.exporter.Exporter;
 import to.rtc.rtc2jira.exporter.jira.entities.Issue;
-import to.rtc.rtc2jira.exporter.jira.entities.Project;
+import to.rtc.rtc2jira.exporter.jira.entities.IssueFields;
+import to.rtc.rtc2jira.exporter.jira.entities.IssueType;
 import to.rtc.rtc2jira.exporter.jira.entities.ProjectOverview;
 import to.rtc.rtc2jira.storage.StorageEngine;
 
 public class JiraExporter implements Exporter {
 
-  private StorageEngine engine;
+  private StorageEngine store;
   private Settings settings;
   private JiraRestAccess restAccess;
+  private List<IssueType> _existingIssueTypes;
 
   @Override
-  public void initialize(Settings settings, StorageEngine engine) {
+  public void initialize(Settings settings, StorageEngine store) {
     this.settings = settings;
-    this.engine = engine;
+    this.store = store;
   }
 
   @Override
@@ -34,7 +45,7 @@ public class JiraExporter implements Exporter {
     if (settings.hasJiraProperties()) {
       restAccess = new JiraRestAccess(settings.getJiraUrl(), settings.getJiraUser(),
           settings.getJiraPassword());
-      ClientResponse response = restAccess.getResponse("/project");
+      ClientResponse response = restAccess.get("/project");
       if (response.getStatus() == Status.OK.getStatusCode()) {
         isConfigured = true;
       } else {
@@ -46,27 +57,104 @@ public class JiraExporter implements Exporter {
 
   @Override
   public void export() throws Exception {
-    List<ProjectOverview> projects =
-        restAccess.get("/project", new GenericType<List<ProjectOverview>>() {});
-
-    Project project = restAccess.get("/project/10001", Project.class);
-
-    Issue issue = restAccess.get("/issue/WOR-2", Issue.class);
-
-    JSONObject data = createIssueData(project);
-    String response = restAccess.post("/issue/", data.toString(), String.class);
+    Optional<ProjectOverview> projectOptional = getProject();
+    if (projectOptional.isPresent()) {
+      for (ODocument workItem : store.getRTCWorkItems()) {
+        Issue issue = createIssueFromWorkItem(workItem, projectOptional.get());
+        Issue jiraIssue = createIssueInJira(issue);
+        storeReference(jiraIssue);
+      }
+    }
   }
 
-  private JSONObject createIssueData(Project project) throws JSONException {
-    JSONObject data = new JSONObject();
-    JSONObject fields = new JSONObject();
-    fields.put("summary", "Test REST");
-    fields.put("description",
-        "Creating of an issue using project keys and issue type names using the REST API");
-    fields.put("project", new JSONObject().put("id", project.getId()));
-    fields.put("issuetype", new JSONObject().put("name", "Task"));
+  private void storeReference(Issue jiraIssue) {
+    // TODO Store key/id in o2 database
+  }
 
-    data.put("fields", fields);
-    return data;
+  private Optional<ProjectOverview> getProject() {
+    List<ProjectOverview> projects =
+        restAccess.get("/project", new GenericType<List<ProjectOverview>>() {});
+    for (ProjectOverview project : projects) {
+      if (project.getKey().equals(settings.getJiraProjectKey())) {
+        return Optional.of(project);
+      }
+    }
+    return Optional.ofNullable(null);
+  }
+
+  private Issue createIssueInJira(Issue issue) throws Exception {
+    ClientResponse postResponse = restAccess.post("/issue", issue);
+    if (postResponse.getStatus() == Status.CREATED.getStatusCode()) {
+      return postResponse.getEntity(Issue.class);
+    } else {
+      System.err.println("Problems while creating issue: " + postResponse.getEntity(String.class));
+      return null;
+    }
+  }
+
+  private Issue createIssueFromWorkItem(ODocument workItem, ProjectOverview project)
+      throws Exception {
+    Issue issue = new Issue();
+    IssueFields issueFields = issue.getFields();
+    issueFields.setProject(project);
+    for (Entry<String, Object> entry : workItem) {
+      String field = entry.getKey();
+      switch (field) {
+        case ID:
+          String id = (String) entry.getValue();
+          issue.setId(id);
+          break;
+        case SUMMARY:
+          String summary = (String) entry.getValue();
+          issueFields.setSummary(summary);
+          break;
+        case DESCRIPTION:
+          String htmlText = (String) entry.getValue();
+          issueFields.setDescription(htmlText);
+          break;
+        case WORK_ITEM_TYPE:
+          String workitemType = (String) entry.getValue();
+          switch (workitemType) {
+            case TASK:
+              issueFields.setIssuetype(getIssueType("Task"));
+              break;
+            case STORY:
+              issueFields.setIssuetype(getIssueType("User Story"));
+              break;
+            case EPIC:
+              issueFields.setIssuetype(getIssueType("Epic"));
+              break;
+            case BUSINESSNEED:
+              issueFields.setIssuetype(getIssueType("Business Need"));
+              break;
+            default:
+              System.out
+                  .println("Cannot determine issuetype for unknown workitemType: " + workitemType);
+              break;
+          }
+          break;
+        default:
+          break;
+      }
+    }
+    return issue;
+  }
+
+  private IssueType getIssueType(String issuetypeName) throws Exception {
+    if (_existingIssueTypes == null) {
+      _existingIssueTypes = restAccess.get("/issuetype", new GenericType<List<IssueType>>() {});
+    }
+    for (IssueType issuetype : _existingIssueTypes) {
+      if (issuetype.getName().equals(issuetypeName)) {
+        return issuetype;
+      }
+    }
+
+    IssueType newIssueType = new IssueType();
+    newIssueType.setName(issuetypeName);
+    IssueType createdIssueType = restAccess.post("/issuetype", newIssueType, IssueType.class);
+    _existingIssueTypes.add(createdIssueType);
+    return newIssueType;
+
   }
 }

48b6e9f8e5ce239105cdc57547b31b048b978a51
Gustaf Hansen <gustaf.hansen@bison-group.com>
2015-09-09 14:02:55 +0200

    add capability for dates without time

--- src/main/java/to/rtc/rtc2jira/exporter/jira/entities/JiraDateStringAdapter.java
+++ src/main/java/to/rtc/rtc2jira/exporter/jira/entities/JiraDateStringAdapter.java
@@ -18,12 +18,19 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
  */
 public class JiraDateStringAdapter extends XmlAdapter<String, Date> {
 
-  public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+  // N.B. this works for timezones without a colon separating hours and minutes (e.g. +0200), which
+  // currently is the case with datetime strings returned by Jira
+  public static final SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+  public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
 
 
   @Override
   public Date unmarshal(String dateAsString) throws Exception {
-    return DATE_FORMAT.parse(dateAsString);
+    if (dateAsString.length() == 10) {
+      return DATE_FORMAT.parse(dateAsString);
+    } else {
+      return DATE_TIME_FORMAT.parse(dateAsString);
+    }
   }
 
   @Override

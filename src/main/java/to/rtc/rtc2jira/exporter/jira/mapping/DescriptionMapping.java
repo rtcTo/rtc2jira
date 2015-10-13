46233528ccb22eeac67f88f6a68a7a4eb5d2da38
Gustaf Hansen <gustaf.hansen@bison-group.com>
2015-10-13 10:58:29 +0200

    description field: map html markup to jira rich text markup

--- src/main/java/to/rtc/rtc2jira/exporter/jira/mapping/DescriptionMapping.java
+++ src/main/java/to/rtc/rtc2jira/exporter/jira/mapping/DescriptionMapping.java
@@ -15,7 +15,30 @@ public class DescriptionMapping implements Mapping {
   @Override
   public void map(Object value, Issue issue, StorageEngine storage) {
     String description = (String) value;
+    // line breaks
     description = description.replaceAll("<br/>", "\r\n");
+    // bold
+    description = description.replaceAll("<b>", "*");
+    description = description.replaceAll("</b>", "*");
+    // italics
+    description = description.replaceAll("<i>", "_");
+    description = description.replaceAll("</i>", "_");
+    // emphasis
+    description = description.replaceAll("<em>", "_");
+    description = description.replaceAll("</em>", "_");
+    // deleted
+    description = description.replaceAll("<del>", "-");
+    description = description.replaceAll("</del>", "-");
+    // inserted
+    description = description.replaceAll("<ins>", "+");
+    description = description.replaceAll("</ins>", "+");
+    // superscript
+    description = description.replaceAll("<sup>", "^");
+    description = description.replaceAll("</sup>", "^");
+    // subscript
+    description = description.replaceAll("<sub>", "~");
+    description = description.replaceAll("</sub>", "~");
+
     issue.getFields().setDescription(description);
   }
 }

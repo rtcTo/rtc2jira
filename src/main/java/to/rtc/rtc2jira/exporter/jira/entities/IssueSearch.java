d40acc5efc4d13fc7a9788297be8f46b851546da
Gustaf Hansen <gustaf.hansen@bison-group.com>
2015-10-16 09:58:49 +0200

    log bad response when doing a jql search

--- src/main/java/to/rtc/rtc2jira/exporter/jira/entities/IssueSearch.java
+++ src/main/java/to/rtc/rtc2jira/exporter/jira/entities/IssueSearch.java
@@ -1,13 +1,19 @@
 package to.rtc.rtc2jira.exporter.jira.entities;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
 import to.rtc.rtc2jira.exporter.jira.JiraExporter;
 import to.rtc.rtc2jira.exporter.jira.JiraRestAccess;
 
+import com.sun.jersey.api.client.ClientResponse;
+
 public class IssueSearch {
+  private static final Logger LOGGER = Logger.getLogger(IssueSearch.class.getName());
+
   public static final IssueSearch INSTANCE;
   private JiraRestAccess restAccess;
 
@@ -20,10 +26,20 @@ public class IssueSearch {
   }
 
   public IssueSearchResult run(String jql) {
-    IssueSearchResult issueSearchResult = restAccess.get("/search?jql=" + jql, IssueSearchResult.class);
+    IssueSearchResult issueSearchResult = new IssueSearchResult();
+    issueSearchResult.issues = Collections.emptyList();
+    issueSearchResult.total = 0;
+    issueSearchResult.startAt = 0;
+    issueSearchResult.maxResults = 0;
+    ClientResponse clientResponse = restAccess.get("/search?jql=" + jql);
+    if (clientResponse.getStatus() == 200) {
+      issueSearchResult = clientResponse.getEntity(IssueSearchResult.class);
+    } else {
+      String responseEntity = clientResponse.getEntity(String.class);
+      LOGGER.warning("JQL search failed. Query string: '" + jql + "'. Response entity: " + responseEntity);
+    }
     return issueSearchResult;
   }
-
 
   @XmlRootElement
   public static class IssueSearchResult {

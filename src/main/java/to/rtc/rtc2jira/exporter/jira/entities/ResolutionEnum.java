99fd4321169039d8b574ab55d522905787e68988
Gustaf Hansen <gustaf.hansen@bison-group.com>
2015-11-24 11:35:04 +0100

    handle rtc resolution id 8

--- src/main/java/to/rtc/rtc2jira/exporter/jira/entities/ResolutionEnum.java
+++ src/main/java/to/rtc/rtc2jira/exporter/jira/entities/ResolutionEnum.java
@@ -8,7 +8,7 @@ import java.util.logging.Logger;
 import to.rtc.rtc2jira.ExportManager;
 
 public enum ResolutionEnum {
-  fixed(1, 1, -1), wont_fix(5, 2, -1), duplicate(2, 3, -1), incomplete(-1, 4, -1), cannotReproduce(4, 5, -1), done(-1,
+  fixed(1, 1, -1), wont_fix(5, 2, -1), duplicate(2, 3, -1), incomplete(8, 4, -1), cannotReproduce(4, 5, -1), done(-1,
       10000, -1), wontDo(3, 10001, -1);
 
 

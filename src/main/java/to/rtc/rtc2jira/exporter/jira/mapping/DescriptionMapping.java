b6e42e472775da72d392a1f6b7680d1e468cb537
Gustaf Hansen <gustaf.hansen@bison-group.com>
2016-02-26 14:40:46 +0100

    translate html anchors to jira anchors

--- src/main/java/to/rtc/rtc2jira/exporter/jira/mapping/DescriptionMapping.java
+++ src/main/java/to/rtc/rtc2jira/exporter/jira/mapping/DescriptionMapping.java
@@ -3,6 +3,17 @@
  */
 package to.rtc.rtc2jira.exporter.jira.mapping;
 
+import java.io.IOException;
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
 import to.rtc.rtc2jira.exporter.jira.entities.Issue;
 import to.rtc.rtc2jira.storage.StorageEngine;
 
@@ -16,11 +27,11 @@ public class DescriptionMapping implements Mapping {
   public void map(Object value, Issue issue, StorageEngine storage) {
     String description = (String) value;
     description = convertHtmlToJiraMarkup(description);
+
     if (description != null) {
       issue.getFields().setDescription(description);
     }
   }
-
 
   public static String convertHtmlToJiraMarkup(String text) {
     if (text != null) {
@@ -48,7 +59,54 @@ public class DescriptionMapping implements Mapping {
       text = text.replaceAll("<sub>", "~");
       text = text.replaceAll("</sub>", "~");
     }
+
+    // handle anchors
+
+    int found = text.indexOf("<a ", 0);
+
+    while (found > -1) {
+      int anchorEnd = text.indexOf("</a>");
+      anchorEnd += 4;
+      Node containerNode = parseXml(text.substring(found));
+      if (containerNode != null) {
+        Node anchor = containerNode.getFirstChild();
+        String textContent = anchor.getTextContent();
+        String link = anchor.getAttributes().getNamedItem("href").getNodeValue();
+        if (textContent != null) {
+          textContent = textContent.trim();
+          if (!textContent.isEmpty()) {
+            textContent += "|";
+          }
+        } else {
+          textContent = "";
+        }
+        String jiraAnchor = "[" + textContent + link + "]";
+        // insert Jira anchor
+        text = text.substring(0, found) + jiraAnchor + (anchorEnd < text.length() ? text.substring(anchorEnd) : "");
+      }
+      found++;
+      found = text.indexOf("<a ", found);
+    }
     return text;
+  }
+
+
+  public static Node parseXml(String fragment) {
+    // Wrap the fragment in an arbitrary element.
+    fragment = "<fragment>" + fragment + "</fragment>";
+    try {
+      // Create a DOM builder and parse the fragment.
+      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+      Document d = factory.newDocumentBuilder().parse(new InputSource(new StringReader(fragment)));
+
+      // Return the fragment.
+      return d.getFirstChild();
+    } catch (SAXException e) {
+      // A parsing error occurred; the XML input is not valid.
+    } catch (ParserConfigurationException e) {
+    } catch (IOException e) {
+    }
+    return null;
   }
 
 }
